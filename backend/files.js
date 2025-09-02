4219a3c85fe01285164228203c247239d890fef0
rfights <159859811+rfights@users.noreply.github.com>
2025-09-02 20:46:01 +0000

    fix: update user email and improve file imports; remove unused server files

--- backend/files.js
+++ backend/files.js
@@ -1,6 +1,5 @@
-import { writeFileSync, readFileSync } from 'fs'
-
-const users = [{ name: 'Adam Ondra', email: 'adam.ondra@limb,ing' }]
+import { writeFileSync, readFileSync } from 'node:fs'
+const users = [{ name: 'Adam Ondra', email: 'adam.ondra@climb.ing' }]
 const usersJson = JSON.stringify(users)
 writeFileSync('backend/users.json', usersJson)
 const readUsersJson = readFileSync('backend/users.json')
