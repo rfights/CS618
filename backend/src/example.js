7a9554817e350060c36d9fc6abf3ca53fa10cf91
rfights <159859811+rfights@users.noreply.github.com>
2025-10-06 14:19:59 +0000

    feat: implement Post model and initialize database with sample data

--- backend/src/example.js
+++ backend/src/example.js
@@ -1,2 +1,17 @@
 import { initDatabase } from './db/init.js'
-initDatabase()
+
+import { Post } from './db/models/post.js'
+
+await initDatabase()
+
+const post = new Post({
+  title: 'Hello Mongoose!',
+  author: 'Ramey Fights',
+  contents: 'This post is stored in a MongoDB database using Mongoose.',
+  tags: ['mongoose', 'mongodb'],
+})
+
+await post.save()
+
+const posts = await Post.find()
+console.log(posts)
