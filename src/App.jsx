a6de11010afe94fe0a25d5b58cbd75223c78a652
rfights <159859811+rfights@users.noreply.github.com>
2025-10-07 18:41:32 +0000

    feat: add CreatePost, PostFilter, and PostSorting components; update App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { PostList } from './components/PostList.jsx'
+import { CreatePost } from './components/CreatePost.jsx'
+import { PostFilter } from './components/PostFilter.jsx'
+import { PostSorting } from './components/PostSorting.jsx'
 
 const posts = [
   {
@@ -6,8 +9,21 @@ const posts = [
     contents: "Let's become full-stack developers!",
     author: 'Ramey Fights',
   },
+  { title: 'Hello React!' },
 ]
 
 export function App() {
-  return <PostList posts={posts} />
+  return (
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter by:
+      <PostFilter field='author' />
+      <br />
+      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
 }
