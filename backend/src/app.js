2366595d7827d3a185822a236fbe108f32b5166a
rfights <159859811+rfights@users.noreply.github.com>
2025-10-07 17:54:45 +0000

    feat: add CORS support to the application

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { postsRoutes } from './routes/posts.js'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 
 postsRoutes(app)
