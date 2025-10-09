44020aab35cb43cfc4af6a42b78d741c56e976ff
rfights <159859811+rfights@users.noreply.github.com>
2025-10-09 18:06:51 +0000

    fix: add missing slash in API endpoint URLs for getPosts and createPost functions

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,13 +1,13 @@
 export const getPosts = async (queryParams) => {
   const res = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}posts?` +
+    `${import.meta.env.VITE_BACKEND_URL}/posts?` +
       new URLSearchParams(queryParams),
   )
   return await res.json()
 }
 
 export const createPost = async (post) => {
-  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}posts`, {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(post),
