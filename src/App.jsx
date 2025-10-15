b0db9298d6ffe19097cd8b057123988d8f45d57a
rfights <159859811+rfights@users.noreply.github.com>
2025-10-15 16:31:02 +0000

    refactor: simplify App component by removing routing and adding children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Blog } from './pages/Blog.jsx'
-import { Signup } from './pages/Signup.jsx'
-
-import { Login } from './pages/Login.jsx'
-
+import PropTypes from 'prop-types'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Blog />,
-  },
-  {
-    path: '/signup',
-    element: <Signup />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-])
-
-export function App() {
+export function App({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <RouterProvider router={router} />
-      </AuthContextProvider>
+      <AuthContextProvider>{children}</AuthContextProvider>
     </QueryClientProvider>
   )
+}
+
+App.propTypes = {
+  children: PropTypes.element.isRequired,
 }
